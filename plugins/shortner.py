03e9df085b916830c365f2fd92c3007e839a1a0e
Himasara-lk <99850880+Himasara-lk@users.noreply.github.com>
2022-02-17 09:10:45 +0530

    Update shortner.py

--- plugins/shortner.py
+++ plugins/shortner.py
@@ -24,7 +24,7 @@ BUTTONS = InlineKeyboardMarkup(
 @Client.on_message(filters.private & filters.regex(r'https?://[^\s]+'))
 async def reply_shortens(bot, update):
     message = await update.reply_text(
-        text="`🔗 Analysing your link\n\nPlease Wait...`",
+        text="`🔗 Analysing your link\n\n🚀 Please Wait...`",
         disable_web_page_preview=True,
         quote=True
     )
